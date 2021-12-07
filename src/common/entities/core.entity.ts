f7c56a675cebcf9f4267a8d1b7ddbd09d4ff3b30
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-08 02:27:33 +0900

    working create Account Mutation

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,3 +1,4 @@
+import { Field } from '@nestjs/graphql';
 import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
@@ -6,11 +7,14 @@ import {
 
 export class CoreEntity {
   @PrimaryGeneratedColumn()
+  @Field((type) => Number)
   id: number;
 
   @CreateDateColumn() // entity를 만들었을때 자동으로 설정해 주는 special column
+  @Field((type) => Date)
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Field((type) => Date)
   updatedAt: Date;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +10,21 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({ email, password, role }: CreateAccountInput) {
+    try {
+      // check new user
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        //make error
+        return;
+      }
+      // create user
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (error) {
+      //make error
+      return;
+    }
+  }
 }
