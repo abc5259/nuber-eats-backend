ccb3479c8589d161c89ccd88acb16bc92c508e29
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-09 03:44:50 +0900

    working user jwt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly users: Repository<User>,
-  ) {}
+    private readonly config: ConfigService,
+  ) {
+    console.log(config);
+  }
 
   //create User
   async createAccount({
@@ -51,6 +56,7 @@ export class UsersService {
       if (!passwordCorrect) {
         return { ok: false, error: 'Wrong password' };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return { ok: true, token: 'goododo' };
     } catch (error) {
       return { ok: false, error };
