0150fb6fc345214e6633b7fd0ee5bab3fdb18546
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-11 05:54:40 +0900

    clear get users with token

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,15 +1,28 @@
-import { NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from './jwt.service';
 
-//implements는 해당 클래스가 interface로 행동하도록 한다.
-// export class JwtMiddleware implements NestMiddleware {
-//   use(req: Request, res: Response, next: NextFunction) {
-//     console.log(req.headers);
-//     next();
-//   }
-// }
-
-export function jwtMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(req.headers);
-  next();
+// implements는 해당 클래스가 interface로 행동하도록 한다.
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt'];
+      const decoded = this.jwtService.verify(token.toString());
+      console.log(decoded);
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.userService.findById(decoded['id']);
+          // console.log(user);
+          // req['user'] = user;
+        } catch (e) {}
+      }
+    }
+    next();
+  }
 }
